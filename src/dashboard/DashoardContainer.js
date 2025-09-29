804789f93069d6e9345d121c87b1e91706a4a41d
Mohamed_Ayman <mohamdayman35@gmail.com>
2025-09-29 20:40:05 +0300

    update

--- src/dashboard/DashoardContainer.js
+++ src/dashboard/DashoardContainer.js
@@ -1,10 +1,10 @@
 import { Outlet } from "react-router-dom";
-import Sidebar from "../components/dashboard/Sidebar";
+import SideBar from "../components/dashboard/SideBar";
 import "../components/dashboard/dashboard.css";
 export default function DashboardContainer() {
   return (
     <div className="d-flex gap-2 mt-4  position-relative overflow-hidden dashboard-conteiner">
-      <Sidebar />
+      <SideBar />
       <Outlet />
     </div>
   );
